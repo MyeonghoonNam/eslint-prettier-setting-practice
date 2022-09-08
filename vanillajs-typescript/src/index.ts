124140bc1c8f745ea4c025261326c9be0ed267f2
MyeongHoonNam <codenmh0822@naver.com>
2022-09-08 21:40:48 +0900

    feat: test typescript, eslint, prettier

---
+++ vanillajs-typescript/src/index.ts
@@ -0,0 +1,11 @@
+import just from './just';
+
+console.log('안녕, 난 index.js야');
+console.log(`나는 just.js에서 온 ${just}야`);
+
+// type MyArray = { age: number; name: string };
+
+// const person1: MyArray = {
+// 	age: 27,
+// 	name: 'hoon',
+// };
---
+++ vanillajs-typescript/src/just.ts
@@ -0,0 +1,3 @@
+const just = 'just';
+
+export default just;

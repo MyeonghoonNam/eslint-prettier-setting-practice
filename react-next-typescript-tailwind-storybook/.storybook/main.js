bef7428cc3ad65acbe953983cf81a03ce9819b27
MyeongHoonNam <codenmh0822@naver.com>
2022-12-27 19:09:13 +0900

    feat : 절대경로 설정

--- react-next-typescript-tailwind-storybook/.storybook/main.js
+++ react-next-typescript-tailwind-storybook/.storybook/main.js
@@ -1,15 +1,27 @@
+const path = require('path');
+
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+	stories: [
+		'../stories/**/*.stories.mdx',
+		'../stories/**/*.stories.@(js|jsx|ts|tsx)',
+	],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+	],
+	framework: '@storybook/react',
+	core: {
+		builder: '@storybook/builder-webpack5',
+	},
+	webpackFinal: async (config) => {
+		config.resolve.alias = {
+			...config.resolve.alias,
+
+			'@pages': path.resolve(__dirname, '../pages'),
+			'@styles': path.resolve(__dirname, '../styles'),
+		};
+
+		return config;
+	},
+};
--- react-next-typescript-tailwind-storybook/.storybook/preview.js
+++ react-next-typescript-tailwind-storybook/.storybook/preview.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css';
+import '@styles/globals.css';
 import * as NextImage from 'next/image';
 
 NextImage.defaultProps = {

3cffbd971f0a7c2e265395a0d299e69cf05ccd54
MyeongHoonNam <codenmh0822@naver.com>
2022-12-27 18:50:48 +0900

    refactor : Image 최적화 코드 간소화

--- react-next-typescript-tailwind-storybook/.storybook/preview.js
+++ react-next-typescript-tailwind-storybook/.storybook/preview.js
@@ -1,12 +1,9 @@
 import '../styles/globals.css';
 import * as NextImage from 'next/image';
 
-const OriginalNextImage = NextImage.default;
-
-Object.defineProperty(NextImage, 'default', {
-	configurable: true,
-	value: (props) => <OriginalNextImage {...props} unoptimized />,
-});
+NextImage.defaultProps = {
+	unoptimized: true,
+};
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },

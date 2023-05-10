6aa345c1341dc9610178f39dc7eb94f44e7d92bd
MyeongHoonNam <codenmh0822@naver.com>
2023-05-10 19:57:04 +0900

    next, typescript, tailwind, twin.macro boilerplate

---
+++ next-typescript-twinmacro/src/pages/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import tw, { css } from 'twin.macro';
+
+const Home = () => {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <h2>max value === 3</h2>
+      <span
+        css={[
+          tw`text-[36px] block`,
+          css`
+            color: ${value === 3 && 'hotpink'};
+          `,
+        ]}
+      >
+        {value}
+      </span>
+      <button
+        type="button"
+        onClick={() => setValue((prev) => prev + 1)}
+        className="text-[36px] bolder-[1px]"
+      >
+        +
+      </button>
+
+      <button
+        type="button"
+        onClick={() => setValue((prev) => prev - 1)}
+        className="text-[36px]"
+      >
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Home;

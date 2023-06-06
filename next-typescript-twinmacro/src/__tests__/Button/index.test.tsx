2d832c9a4652bc25bb83f81b99031233560ce9e0
MyeongHoonNam <codenmh0822@naver.com>
2023-06-06 16:05:06 +0900

    test : increment Button 컴포넌트  렌더링 테스트

---
+++ next-typescript-twinmacro/src/__tests__/Button/index.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { Button } from '@/components';
+import { CONTAINER_ID } from '@/constants/Button';
+
+describe('Button Component', () => {
+  it('Increment Button Render', () => {
+    const text = 'Plus';
+
+    const props = {
+      text,
+    };
+
+    render(<Button {...props} />);
+
+    const container = screen.getByTestId(CONTAINER_ID);
+
+    expect(container).toBeInTheDocument();
+    expect(container.textContent).toBe(text);
+  });
+});

abbc484e56f397a8fe223fc479147266940c0c4f
MyeongHoonNam <codenmh0822@naver.com>
2023-06-07 14:19:06 +0900

    refactor : button 컴포넌트 렌더링 테스트 간소화

--- next-typescript-twinmacro/src/__tests__/Button/index.test.tsx
+++ next-typescript-twinmacro/src/__tests__/Button/index.test.tsx
@@ -1,25 +1,11 @@
 import { render, screen } from '@testing-library/react';
+
 import { Button } from '@/components';
 import { CONTAINER_ID } from '@/constants/Button';
 
 describe('Button Component', () => {
-  it('Increment Button Render', () => {
-    const text = 'Plus';
-
-    const props = {
-      text,
-    };
-
-    render(<Button {...props} />);
-
-    const container = screen.getByTestId(CONTAINER_ID);
-
-    expect(container).toBeInTheDocument();
-    expect(container.textContent).toBe(text);
-  });
-
-  it('Decrement Button Render', () => {
-    const text = 'Minus';
+  it('button render', () => {
+    const text = 'button text';
 
     const props = {
       text,

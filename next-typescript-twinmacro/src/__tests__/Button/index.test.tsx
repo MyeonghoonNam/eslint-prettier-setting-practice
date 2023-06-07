3d03ebc3f04afd7f231e7d305970c8ca89cbaf32
MyeongHoonNam <codenmh0822@naver.com>
2023-06-07 14:46:13 +0900

    test : Button 컴포넌트 함수 호출 여부 테스트

--- next-typescript-twinmacro/src/__tests__/Button/index.test.tsx
+++ next-typescript-twinmacro/src/__tests__/Button/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import { Button } from '@/components';
 import { CONTAINER_ID } from '@/constants/Button';
@@ -17,5 +18,23 @@ describe('Button Component', () => {
 
     expect(container).toBeInTheDocument();
     expect(container.textContent).toBe(text);
+  });
+
+  it('is the function called', async () => {
+    const text = 'text';
+    const onClick = jest.fn();
+
+    const props = {
+      text,
+      onClick,
+    };
+
+    render(<Button {...props} />);
+
+    const container = screen.getByTestId(CONTAINER_ID);
+
+    await userEvent.click(container);
+
+    expect(onClick).toBeCalled();
   });
 });
